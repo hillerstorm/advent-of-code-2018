dd6a981516b18f0fd57e880fecde9f626a81205f
Johan Hillerström <progr@mmer.nu>
2018-12-12 23:44:03 +0100

    Add example input as test for day 12

--- AdventOfCode.Tests/Day12Tests.cs
+++ AdventOfCode.Tests/Day12Tests.cs
@@ -10,7 +10,8 @@ namespace AdventOfCode.Tests
         {
             var assertions = new[]
             {
-                ("Inputs/Day12.txt", 3890),
+                ("Inputs/Day12_example.txt", 325),
+                ("Inputs/Day12_mine.txt", 3890),
             };
             assertions.ForEach(((string Input, int Expected) x) =>
                 Assert.Equal(x.Expected, Day12.Part1(x.Input.ReadLines())));
@@ -21,7 +22,8 @@ namespace AdventOfCode.Tests
         {
             var assertions = new[]
             {
-                ("Inputs/Day12.txt", 4800000001087),
+                ("Inputs/Day12_example.txt", 50000000501),
+                ("Inputs/Day12_mine.txt", 4800000001087),
             };
             assertions.ForEach(((string Input, long Expected) x) =>
                 Assert.Equal(x.Expected, Day12.Part2(x.Input.ReadLines())));
